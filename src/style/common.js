56e0fcde2cda1be6b576a32c4a152ce7270f0fed
Baek0826 <ffhjkl1230@naver.com>
2024-10-10 17:46:41 +0900

    Fix: 글꼴 오류 수정

--- src/style/common.js
+++ src/style/common.js
@@ -1,7 +1,7 @@
 import { css } from "@emotion/react";
 
 export const reset = css`
-    
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap');
 
     html, body, #root {
         margin: 0;

712724cf2deca7722c73fe83514bf3134e71a512
Baek0826 <ffhjkl1230@naver.com>
2024-10-28 21:04:19 +0800

    Design: 마이페이지 디자인

---
+++ src/pages/userpage/ModifyProfilePage/ModifyProfilePage.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from 'react';
+/** @jsxImportSource @emotion/react */
+import * as s from "./style";
+import UserSubLayout from '../../../components/usercomponents/UserSubLayout/UserSubLayout';
+import UserSubContainer from '../../../components/usercomponents/UserSubContainer/UserSubContainer';
+import BackButton from '../../../components/usercomponents/BackButton/BackButton';
+import { useMutation, useQueryClient } from 'react-query';
+import { v4 as uuid } from 'uuid'
+import Swal from 'sweetalert2';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '../../../firebase/firebase';
+import { instance } from '../../../apis/utils/instance';
+
+function ModifyProfilePage(props) {
+    const [isShow, setShow] = useState(true);
+
+    const queryClient = useQueryClient();
+    const userInfo = queryClient.getQueryData("userInfoQuery");
+
+    const [userInput, setUserInput] = useState(userInfo?.data);
+
+    const modifyUser = useMutation(
+        async () => await instance.put("/user", userInput),
+        {
+            onSuccess: response => {
+                Swal.fire({
+                    icon: 'success',
+                    text: '수정 완료',
+                    backdrop: false,
+                    showConfirmButton: false,
+                    timer: 1000,
+                    willClose: () => {
+                        queryClient.invalidateQueries("userInfoQuery");
+                    },
+                    customClass: {
+                        popup: 'custom-timer-swal',
+                        container: 'container'
+                    }
+                })
+            }
+        }
+    )
+
+    const modifyUserProfileImgOnClick = () => {
+        Swal.fire({
+            icon: 'question',
+            text: '사진을 변경하시겠습니까?',
+            backdrop: false,
+            showCancelButton: true,
+            confirmButtonText: '확인',
+            cancelButtonText: '취소',
+            customClass: {
+                popup: 'custom-confirm-swal',
+                container: 'container',
+                confirmButton: 'confirmButton',
+            }
+        }).then(result => {
+            if (result.isConfirmed) {
+                const fileInput = document.createElement("input");
+                fileInput.setAttribute("type", "file");
+                fileInput.setAttribute("accept", "image/*");
+                fileInput.click();
+
+                fileInput.onchange = (e) => {
+                    const reviewImage = Array.from(e.target.files)[0];
+                    const storageRef = ref(storage, `user/profile/${uuid()}_${reviewImage.name}`);
+                    const uploadTask = uploadBytesResumable(storageRef, reviewImage);
+                    uploadTask.on(
+                        "state_changed",
+                        (snapshot) => {
+                            Swal.fire({
+                                text: '사진 등록 중...',
+                                backdrop: false,
+                                allowOutsideClick: false,
+                                didOpen: () => {
+                                    Swal.showLoading();
+                                },
+                                customClass: {
+                                    popup: 'custom-loading-swal',
+                                    container: 'container'
+                                }
+                            });
+                        },
+                        (error) => {
+                            console.error(error);
+                        },
+                        async (success) => {
+                            Swal.close();
+                            const url = await getDownloadURL(storageRef);
+                            setUserInput(userInput => ({
+                                ...userInput,
+                                img: url
+                            }))
+                        }
+                    );
+                }
+            }
+        })
+    }
+
+    const handleUserOnChange = (e) => {
+        setUserInput(userInput => ({
+            ...userInput,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleChangeStateOnClick = () => {
+        setUserInput(userInfo?.data);
+    }
+
+    return (
+        <UserSubLayout isShow={isShow}>
+            <UserSubContainer>
+                <BackButton setShow={setShow} title={"내 정보 수정"} />
+                <div css={s.layout}>
+                    <img src={userInfo?.data?.img} onClick={modifyUserProfileImgOnClick} alt="" />
+                    <div css={s.userInfo}>
+                    <p>{userInfo?.data?.name}</p>
+                    <p>{userInfo?.data?.email}</p>
+                </div>
+                <div css={s.inputBox}>
+                    <input type="text" name="name" onChange={handleUserOnChange} value={userInput?.name} />
+                    <input type="text" name="phoneNumber" onChange={handleUserOnChange} value={userInput?.phoneNumber} />
+                </div>
+                <div css={s.buttonBox}>
+                    <div>
+                        <button onClick={() => modifyUser.mutateAsync().catch(() => { })} disabled={JSON.stringify(userInput) === JSON.stringify(userInfo?.data)}>확인</button>
+                    </div>
+                    <div>
+                        <button onClick={handleChangeStateOnClick}>취소</button>
+                    </div>
+                </div>
+            </div>
+        </UserSubContainer>
+        </UserSubLayout >
+    );
+}
+
+export default ModifyProfilePage;
--- src/pages/userpage/ModifyProfilePage/style.js
+++ src/pages/userpage/ModifyProfilePage/style.js
@@ -29,7 +29,7 @@ export const userInfo = css`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin-bottom: 5px;
+    margin-bottom: 20px;
     border-radius: 10px;
     padding: 5px;
     width: 100%;
@@ -44,37 +44,30 @@ export const userInfo = css`
     }
 `;
 
-export const userButtonBox = css`
+export const inputBox = css`
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
     flex-grow: 1;
     width: 100%;
-`;
 
-export const userModify = css`
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 100px;
-    width: 100%;
-
-    & > div {
+    & > input {
         box-sizing: border-box;
         display: flex;
-        height: 50px;
+        border-radius: 10px;
+        padding: 0px 20px;
+        width: 100%;
+        font-size: 16px;
+        height: 60px;
+        background-color: #f1f0f3;
+    }
 
-        & > button {
-            box-sizing: border-box;
-            flex-grow: 1;
-            border: 1px solid #dbdbdb;
-            border-radius: 10px;
-            padding: 10px 10px;
-        }
+    & > input:nth-of-type(1) {
+        margin-bottom: 20px;
     }
 `;
 
-export const userRecord = css`
+export const buttonBox = css`
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
@@ -90,50 +83,20 @@ export const userRecord = css`
             flex-grow: 1;
             border: 1px solid #dbdbdb;
             padding: 10px 10px;
+            color: red;
         }
-    }
 
-    & > div:nth-of-type(1) > button:nth-of-type(1) {
-        border-bottom: none;
-        border-top-right-radius: 10px;
-        border-top-left-radius: 10px;
-    }
-    & > div:nth-of-type(2) > button:nth-of-type(1) {
-        border-bottom-right-radius: 10px;
-        border-bottom-left-radius: 10px;
-    }
-`
-
-export const logoutBox = css`
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-
-    & > div {
-        box-sizing: border-box;
-        display: flex;
-        height: 50px;
-
-        & > button {
-            box-sizing: border-box;
-            flex-grow: 1;
-            border: 1px solid #dbdbdb;
-            padding: 10px 10px;
-        }
     }
     
     & > div:nth-of-type(1) > button:nth-of-type(1) {
         border-bottom: none;
         border-top-right-radius: 10px;
         border-top-left-radius: 10px;
-        color: red;
-        
+        color: #000000;
     }
     & > div:nth-of-type(2) > button:nth-of-type(1) {
         border-bottom-right-radius: 10px;
         border-bottom-left-radius: 10px;
-        color: #777777;
+        color: red;
     }
-
 `;
